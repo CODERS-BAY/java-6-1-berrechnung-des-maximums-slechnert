f7054d55ddba7a40306bf6829ad8efd27071f0a0
Simon <s.lechner.biz@gmail.com>
2020-02-12 11:55:53 +0100

    HOOOORAAAAY, finally something that seems to work as intended, took only 3,5hours, piece of cake

--- src/com/coders/bay/Array.java
+++ src/com/coders/bay/Array.java
@@ -1,29 +1,52 @@
 package com.coders.bay;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Array {
     public static void main(String[] args) {
-        int result = 0;
-        String num = "0";
-        int[] zahlen;
+
+        String num;
 
         Scanner sc = new Scanner(System.in);
-
+        int p;
+        int result;
+        int[] zahlen = new int[0];
 
         for (int i = 0; i >= 0; i++) {
 
-
             System.out.println("Bitte Zahl eingeben");
             num = sc.nextLine();
+
             if (num.contains("q")) {
                 System.out.println("ABBRUCH");
+                break;
             } else {
                 result = Integer.parseInt(num);
-                zahlen = new int[result];
+                int[] zahlenNeu = new int[i + 1];
 
+                for (p = 0; p < i; p++) {
+                    zahlenNeu[p] = zahlen[p];
+                }
+                zahlenNeu[i] = result;
+                zahlen = zahlenNeu;
+            }
+
+        }
+
+        System.out.println(Arrays.toString(zahlen));
+
+        int k;
+        int maximumContainer = 0;
+
+        for (k = 0; k < zahlen.length; k++) {
+            if (zahlen[k] >= maximumContainer) {
+                maximumContainer = zahlen[k];
             }
         }
+        System.out.println("Maximum ist: " + maximumContainer);
+
     }
+
 
 }
